c20e7b2c35069bd1139d1fd41f07dbce4e96840c
MesiAnandaPutri <mesianandaputri24@gmail.com>
2024-12-23 19:25:30 +0700

    memproses data formulir yang dikirimkan dan menampilkan formulir login melalui login.html

--- app.py
+++ app.py
@@ -7,3 +7,19 @@ app = Flask(__name__)
 @app.route('/success/<name>')
 def success(name):
     return f'<h1>Halo {name}! Gimana kabarnya hari ini? 😊</h1>'
+
+# Login route
+# Memproses data formulir yang dikirimkan dan Menampilkan formulir login melalui login.html.
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form.get('nm')  # Fetch 'nm' from form
+        if user:
+            return redirect(url_for('success', name=user))
+        else:
+            return "Name not provided in form data!", 400
+    else:
+        return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
