c2079b6adf9ad0b3b9ccbf29540372b498ae6fd2
MesiAnandaPutri <mesianandaputri24@gmail.com>
2024-12-23 19:24:40 +0700

    menampilkan pesan sukses dengan nama pengguna yang dimasukan

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 from flask import Flask, redirect, url_for, request, render_template
 
 app = Flask(__name__)
+
+# Success route
+#Route ini menampilkan pesan sukses dengan nama pengguna yang dimasukkan.
+@app.route('/success/<name>')
+def success(name):
+    return f'<h1>Halo {name}! Gimana kabarnya hari ini? 😊</h1>'
